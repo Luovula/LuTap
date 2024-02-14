5cb3cc125c79ee5b08be8ae8ba04d40b331fe062
Lou <lou.yiw@gmail.com>
2024-02-14 23:59:55 +0800

    fix(home): add buttons

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,22 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Button } from "@mui/material";
+import ExpandedBanner from "../assets/ExpandedBanner.jpg";
 import Banner from "../assets/Banner.jpg";
 import Footer from "../components/Footer";
+import { useWindowSize } from "../hooks";
 
-const News = {
-  name: "Gospel meets Jazz",
-  date: "2023/11/13",
-  time: "19:00",
-  location: "Chung Yuan Christian University, Concert Hall",
-};
+
 
 const Home: React.FC = () => {
+  const { width } = useWindowSize();
   return (
     <Box className="HomePage">
-      <img className="Banner" src={Banner} alt="Banner" />
-      <Box className="TagBox">
+      <img
+        className={`Banner ${width < 800 ? "Expanded" : ""}`}
+        src={width < 800 ? ExpandedBanner : Banner}
+        alt="Banner"
+      />
+      {/* <Box className="TagBox">
         <Box className="TagBox">Upcoming Events</Box>
       </Box>
       <Box className="NewsBanner">
@@ -23,22 +25,28 @@ const Home: React.FC = () => {
           <Box className="Time"> {News.date + " " + News.time}</Box>
           <Box className="Location"> {News.location}</Box>
         </Box>
-      </Box>
+      </Box> */}
       <Box className="Intro">
         Experience the Authentic and Rooted essence of tap dance, a living
         embodiment of the rhythmic soul of jazz music and the Africanist
         aesthetics that define this rich Black American art form.
       </Box>
-      <Box className="Reel">
-        <Box className="File">reel</Box>
-      </Box>
+      <Button className="SignUp"  onClick={()=>{window.open('link','_blank')}}>Sign up for classes</Button>
       <Box className="SubIntro">
         I offer tap dance and body percussion classes in Taipei. I'm available
         for collaboration, workshops, and choreography projects.
       </Box>
+      <Button className="Collab" onClick={()=>{window.open('link','_blank')}}>Let's Collaborate!</Button>
       <Footer />
     </Box>
   );
 };
 
 export default Home;
+
+// const News = {
+//   name: "Gospel meets Jazz",
+//   date: "2023/11/13",
+//   time: "19:00",
+//   location: "Chung Yuan Christian University, Concert Hall",
+// };
