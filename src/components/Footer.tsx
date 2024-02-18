cf49fcd1654a7628f17414a64ad09079fc5ed3ea
Lou <lou.yiw@gmail.com>
2024-02-19 00:50:14 +0800

    fix(footer): add social media icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import { Box } from "@mui/material";
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Footer: React.FC = () => {
-  return <Box className="Footer">Made by thislou.com</Box>;
+  return <Box className="Footer">
+      <InstagramIcon className="Icon" onClick={()=>{window.open('https://www.instagram.com/lunathehoofer?igsh=N3Y2b3Fpdnp1MDZr&utm_source=qr','_blank')}}/>
+      <YouTubeIcon className="Icon" onClick={()=>{window.open('','_blank')}}/>
+      <LinkedInIcon className="Icon" onClick={()=>{window.open('','_blank')}}/>
+    </Box>;
 };
 
 export default Footer;
