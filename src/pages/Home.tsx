9f84d8bc955ab46caa4e30d38773ca05ffd601ba
Luna Luovula <luna@Lunas-MBP.home>
2024-09-24 20:06:45 +0800

    fix(home): add news banner back

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,15 +12,12 @@ const Home: React.FC = () => {
   const [newsItem, setNewsItem] = useState<{ Details: string[]; Title: string; Link: string } | null>(null);
 
   useEffect(() => {
-    // Create a reference to the specific document in the "News" collection
-    const docRef = doc(db, "News", "Current"); // Replace YOUR_DOCUMENT_ID with the actual ID
+    const docRef = doc(db, "News", "Current"); 
 
-    // Fetch the document
     const fetchDoc = async () => {
       const docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        // Extract the data if the document exists
         setNewsItem(docSnap.data() as { Details: string[]; Title: string; Link: string });
       } else {
         console.log("No such document!");
@@ -29,50 +26,28 @@ const Home: React.FC = () => {
 
     fetchDoc();
   }, []);
-  
+  const News = {
+    name: "Gospel meets Jazz",
+    date: "2023/11/13",
+    time: "19:00",
+    location: "Chung Yuan Christian University, Concert Hall",
+  };
   return (
     <Box className="HomePage">
       <Box className='BannerBox'>
-         <img
-        className={`Banner ${width < 800 ? "Expanded" : ""}`}
-        src={width===0?Banner:width < 800 ? ExpandedBanner : Banner}
-        height='600' width='400'
-        alt="Banner"
-      />
-       <Box className={`OverText ${width < 800 ? "SmallerScreen" : ""}`}>
-        Experience the Authentic and Rooted essence of tap dance, a living
-        embodiment of the rhythmic soul of jazz music and the Africanist
-        aesthetics that define this rich Black American art form.
+        <img
+          className={`Banner ${width < 800 ? "Expanded" : ""}`}
+          src={width === 0 ? Banner : width < 800 ? ExpandedBanner : Banner}
+          height='600' width='400'
+          alt="Banner"
+        />
+        <Box className={`OverText ${width < 800 ? "SmallerScreen" : ""}`}>
+          Experience the Authentic and Rooted essence of tap dance, a living
+          embodiment of the rhythmic soul of jazz music and the Africanist
+          aesthetics that define this rich Black American art form.
         </Box>
-      </Box>   
-      {newsItem&&<Box className="News">
-        <Box className='Title'>{newsItem.Title}</Box>
-        <Box className='Content'>
-          {newsItem.Details.map((detail,idx)=><Box className='Details' key={idx}>{detail}</Box>)}
-        </Box>
-        <Box className='EventFooter'>
-          More details (<a className="Link" href={!!newsItem.Link?newsItem.Link:''}>here</a>) links to the classes and events page.
-        </Box>      
-      </Box>}
-      <Box className="SubIntro">
-        I offer tap dance and body percussion classes in Taipei. I'm available
-        for collaboration, performance, workshops, and choreography projects.
       </Box>
-      <Button className="Collab" onClick={()=>{window.open('link','_blank')}}>Let's Collaborate!</Button>
-      <Footer />
-    </Box>
-  );
-};
-
-export default Home;
-
-// const News = {
-//   name: "Gospel meets Jazz",
-//   date: "2023/11/13",
-//   time: "19:00",
-//   location: "Chung Yuan Christian University, Concert Hall",
-// };
-  {/* <Box className="TagBox">
+      <Box className="TagBox">
         <Box className="TagBox">Upcoming Events</Box>
       </Box>
       <Box className="NewsBanner">
@@ -81,4 +56,24 @@ export default Home;
           <Box className="Time"> {News.date + " " + News.time}</Box>
           <Box className="Location"> {News.location}</Box>
         </Box>
-      </Box> */}
+      </Box>
+      {newsItem && <Box className="News">
+        <Box className='Title'>{newsItem.Title}</Box>
+        <Box className='Content'>
+          {newsItem.Details.map((detail, idx) => <Box className='Details' key={idx}>{detail}</Box>)}
+        </Box>
+        <Box className='EventFooter'>
+          More details (<a className="Link" href={!!newsItem.Link ? newsItem.Link : ''}>here</a>) links to the classes and events page.
+        </Box>
+      </Box>}
+      <Box className="SubIntro">
+        I offer tap dance and body percussion classes in Taipei. I'm available
+        for collaboration, performance, workshops, and choreography projects.
+      </Box>
+      <Button className="Collab" onClick={() => { window.open('link', '_blank') }}>Let's Collaborate!</Button>
+      <Footer />
+    </Box>
+  );
+};
+
+export default Home;
